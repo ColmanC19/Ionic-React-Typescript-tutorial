4b256fa2826364e68d5c2bbee151963ed39ba5d1
Colman Currie <curriecolman@gmail.com>
2020-03-31 12:01:35 -0700

    create app page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet } from '@ionic/react';
+import { IonApp, IonRouterOutlet, IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
+import History from './pages/History';
+import { calendar as calendarIcon, happy as happyIcon } from 'ionicons/icons';
+
+
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -23,15 +27,38 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+/* Third-party styles */
+import "emoji-mart/css/emoji-mart.css";
+
+
 const App: React.FC = () => (
   <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route path="/home" component={Home} exact={true} />
-        <Route exact path="/" render={() => <Redirect to="/home" />} />
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
+  <IonReactRouter>
+      <IonTabs>
+          <IonRouterOutlet>
+              <Route path="/today" component={Home} exact={true} />
+              <Route path="/history" component={History} exact={true} />
+              <Route
+                  exact
+                  path="/"
+                  render={() => <Redirect to="/today" />}
+              />
+          </IonRouterOutlet>
+
+          <IonTabBar slot="bottom">
+              <IonTabButton tab="today" href="/today">
+                  <IonIcon icon={calendarIcon} />
+                  <IonLabel>Today</IonLabel>
+              </IonTabButton>
+
+              <IonTabButton tab="history" href="/history">
+                  <IonIcon icon={happyIcon} />
+                  <IonLabel>History</IonLabel>
+              </IonTabButton>
+          </IonTabBar>
+      </IonTabs>
+  </IonReactRouter>
+</IonApp>
 );
 
 export default App;
