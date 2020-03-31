a3361142386bf28fad13e8f6e2aff1118f21d23a
Colman Currie <curriecolman@gmail.com>
2020-03-31 12:02:35 -0700

    everything else

---
+++ src/pages/History.tsx
@@ -0,0 +1,14 @@
+import { IonContent, IonPage } from '@ionic/react';
+import React from 'react';
+
+const History: React.FC = () => {
+    return (
+        <IonPage>
+            <IonContent className="ion-padding">
+                <p>History page</p>
+            </IonContent>
+        </IonPage>
+    );
+};
+
+export default History;
